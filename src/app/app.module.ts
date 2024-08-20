5aba71ae12b489f542e87ae4c578ff317804ac36
IldoRamos <ildoramosvieira@gmail.com>
2024-08-20 17:36:31 -0300

    html e css para header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,12 +34,14 @@ import { MatCardModule } from '@angular/material/card';
 //Componentes de projeto
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
