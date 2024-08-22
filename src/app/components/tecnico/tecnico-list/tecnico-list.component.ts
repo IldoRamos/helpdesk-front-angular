9327a3e8791c054e13ab1bd4499c0fefefb8624f
IldoRamos <ildoramosvieira@gmail.com>
2024-08-22 09:54:50 -0300

    html, css e models para Tecnico

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from '../../../models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrl: './tecnico-list.component.css'
 })
-export class TecnicoListComponent {
+export class TecnicoListComponent  implements OnInit{
+  ELEMENT_DATA: Tecnico[]= [
+    {
+    id:1,
+    nome: 'Ildo Ramos',
+    cpf: '123.456.789-28',
+    email: 'ramos@gmail.com',
+    senha: '1234',
+    perfis: ['0'],
+    dataCriacao: '13/08/2024'
+  }
+]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
+  constructor(){}
+  ngOnInit(): void {
+      
+  }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
